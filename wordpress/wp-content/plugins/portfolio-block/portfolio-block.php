fdd05e5f005ae08aa14e2a4d7c22a6c3de836ab5
Robbie Sherre <robbiesherre@idesigntek.com>
2025-07-19 16:06:32 -0700

    Portfolio Block API by Robbie Sherre version1.0

---
+++ wordpress/wp-content/plugins/portfolio-block/portfolio-block.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Portfolio Block API
+ * Description: Expose raw block content in the REST API.
+ * Version:     1.0
+ * Author:      Robbie Sherre
+ */
+
+add_filter( 'rest_prepare_post', function( $response, $post, $request ) {
+    $raw = get_post_field( 'post_content', $post );
+    $data = $response->get_data();
+    $data['content']['raw'] = $raw;
+    $response->set_data( $data );
+    return $response;
+}, 10, 3 );
